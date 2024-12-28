2f3d1e716845530b79abab996d94842161a2b082
joshua sack <joshsack99@gmail.com>
2024-12-28 15:20:14 -0500

    fixed routing bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import { SignInButton } from '@clerk/nextjs'
-import { api, HydrateClient } from "~/trpc/server";
+import React from 'react'
 
 export default async function Home() {
-
-  void api.post.getLatest.prefetch();
-
   return (
-    <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <SignInButton />
       </main>
-    </HydrateClient>
   );
-}
+} 
