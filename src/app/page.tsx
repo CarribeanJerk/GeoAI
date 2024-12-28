c706e57f04a998c43a45b1b6a7f27623b421b809
joshua sack <joshsack99@gmail.com>
2024-12-28 15:37:18 -0500

    added more clerk func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import { SignInButton } from '@clerk/nextjs'
+"use client"
+
+import { SignInButton, SignOutButton, useUser } from '@clerk/nextjs'
 import React from 'react'
 
-export default async function Home() {
+export default function Home() {
+  const user = useUser();
+
   return (
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <SignInButton />
+        <div>
+          {!user.isSignedIn && <SignInButton />}
+          {!!user.isSignedIn && <SignOutButton />}
+        </div>
       </main>
   );
-} 
+}
